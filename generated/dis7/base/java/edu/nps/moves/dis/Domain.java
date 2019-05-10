12653df487d70c3d818ce5b37d84df3e79eab7ba
Mike Bailey <jmbailey@nps.edu>
2019-05-10 12:43:31 -0700

    For entities and jammers

---
+++ generated/dis7/base/java/edu/nps/moves/dis/Domain.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.nps.moves.dis;
+
+/**
+ * Domain.java created on May 7, 2019
+ * MOVES Institute Naval Postgraduate School, Monterey, CA, USA www.nps.edu
+ *
+ * Marker interface to polymorphize domain field in Pdus
+ * @author Mike Bailey, jmbailey@nps.edu
+ * @version $Id$
+ */
+public interface Domain extends Marshaller
+{
+}
